c35c1c21c1289c5811beb1191d6d8e546c8ba083
Zhanna <nesterova.zhannet@gmail.com>
2022-07-25 16:06:39 +0500

    top-slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
+'use strict';
+
+import { tns } from 'tiny-slider';
+
+tns({
+    container: '.slider_content',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    speed:500,
+    navPosition:'bottom',
+    controlsContainer:'#customize-controls',
+    autoplayButton:'#autoplay',
+    navContainer:'#nav_slider'
+  });
+
 console.log("Fasad");
